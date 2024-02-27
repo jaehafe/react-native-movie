0ffaeef0193a3959759d3baf7ccc711e69248c97
Jaeha Lee <wogkdkrm112@gmail.com>
2024-02-28 00:05:41 +0900

    feat: React Navigation - dependencies 설치

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
