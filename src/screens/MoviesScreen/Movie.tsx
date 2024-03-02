5889010fac241c9eb58f14c4b9f12b953260bac2
Jaeha Lee <wogkdkrm112@gmail.com>
2024-03-03 00:10:11 +0900

    feat: MovieScreen styling

--- src/screens/MoviesScreen/Movie.tsx
+++ src/screens/MoviesScreen/Movie.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Colors from 'open-color';
 
 interface MovieProps {
   title: string;
   originalTitle: string;
   releaseDate: string;
   overview: string;
-  postUrl?: string;
+  posterUrl?: string | null;
 }
 
 export default function Movie({
@@ -14,16 +15,64 @@ export default function Movie({
   originalTitle,
   releaseDate,
   overview,
-  postUrl,
+  posterUrl,
 }: MovieProps) {
   return (
     <View style={styles.container}>
-      <View style={styles.poster}></View>
+      <View style={styles.poster}>
+        {posterUrl !== null && (
+          <Image style={styles.posterImage} source={{ uri: posterUrl }} />
+        )}
+      </View>
+      <View style={styles.info}>
+        <Text style={styles.titleText}>{title}</Text>
+        <Text style={styles.originalTitleText}>{originalTitle}</Text>
+        <Text style={styles.releaseDateText}>{releaseDate}</Text>
+        <Text style={styles.overViewText}>{overview}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  poster: {},
+  container: {
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 12,
+    flexDirection: 'row',
+    borderColor: Colors.gray[6],
+  },
+  poster: {
+    width: 100,
+    height: 150,
+    backgroundColor: Colors.gray[3],
+  },
+  posterImage: {
+    width: 100,
+    height: 150,
+  },
+  info: {
+    marginLeft: 12,
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: 'bold',
+  },
+  originalTitleText: {
+    marginTop: 2,
+    fontSize: 16,
+    color: Colors.white,
+  },
+  releaseDateText: {
+    marginTop: 2,
+    fontSize: 14,
+    color: Colors.white,
+  },
+  overViewText: {
+    marginTop: 8,
+    fontSize: 12,
+    color: Colors.white,
+  },
 });
--- src/screens/MoviesScreen/MoviesScreen.tsx
+++ src/screens/MoviesScreen/MoviesScreen.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
 import useMovies from './hooks/useMovies';
 import Movie from './Movie';
+import Colors from 'open-color';
 
 export default function HomeScreen() {
   const { movies } = useMovies();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        barStyle={Platform.OS === 'ios' ? 'light-content' : 'dark-content'}
+      />
       <FlatList
-        style={styles.movieList}
+        contentContainerStyle={styles.movieList}
         data={movies}
         renderItem={({ item: movie }) => {
           const { title, originalTitle, releaseDate, overview, posterUrl } =
             movie;
           return <Movie {...movie} />;
         }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  movieList: {},
+  container: {
+    flex: 1,
+    backgroundColor: Colors.black,
+  },
+  movieList: {
+    padding: 10,
+  },
+  separator: {
+    height: 16,
+  },
 });
