4d20068a303cfc30a2d75437e85afdea6d3b40b9
Jaeha Lee <wogkdkrm112@gmail.com>
2024-03-03 00:53:25 +0900

    feat: axios instance 생성, .env import

---
+++ src/modules/ApiRequest.ts
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { API_KEY } from '@env';
+import { Movie } from 'src/@types';
+
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: API_KEY,
+    language: 'ko-KR',
+  },
+});
+
+interface MovieResponse {
+  poster_path: string | null;
+  overview: string;
+  release_date: string;
+  id: number;
+  original_title: string;
+  title: string;
+}
+
+interface GetDiscoverMovieResponse {
+  page: number;
+  results: MovieResponse[];
+  total_results: number;
+  total_pages: number;
+}
+
+interface GetDiscoverMoviesParams {
+  releaseDateGte?: string;
+  releaseDateLte?: string;
+}
+
+export const getDiscoverMovies = async ({
+  releaseDateGte,
+  releaseDateLte,
+}: GetDiscoverMoviesParams) => {
+  const response = await instance.get<GetDiscoverMovieResponse>(
+    'discover/movie',
+    {
+      params: {
+        ['release_date.gte']: releaseDateGte,
+        ['release_date.lte']: releaseDateLte,
+      },
+    },
+  );
+
+  const movies: Movie[] = response.data.results.map<Movie>(r => ({
+    id: r.id,
+    title: r.title,
+    originalTitle: r.original_title,
+    releaseDate: r.release_date,
+    overview: r.overview,
+    posterUrl:
+      r.poster_path !== null ? `${IMG_BASE_URL}/${r.poster_path}` : null,
+  }));
+
+  return {
+    page: response.data.page,
+    results: movies,
+    totalPages: response.data.total_pages,
+    totalResults: response.data.total_results,
+  };
+};
